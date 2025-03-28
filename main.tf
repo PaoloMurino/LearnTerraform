21163449c89e6d239bcef67a624c19c664b802d7
PaoloMurino <paolomurino19@gmail.com>
2025-03-28 16:32:53 +0100

    Parametrizzazione e creazione vm, bucket e sa

--- main.tf
+++ main.tf
@@ -1,29 +1,21 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "6.8.0"
-    }
-  }
-}
-
 provider "google" {
-  project = "test-pmurino-02"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
 
 resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+  name = var.vpc
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
+  name         = var.vm.name
+  machine_type = var.vm.machine_type
+  tags         = var.vm.tags
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = var.vm.image
     }
   }
 
@@ -32,4 +24,45 @@ resource "google_compute_instance" "vm_instance" {
     access_config {
     }
   }
+
+  service_account {
+    email  = google_service_account.vm_sa.email
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+}
+
+resource "google_storage_bucket" "my_bucket" {
+  name          = var.bucket.name
+  location      = var.bucket.location
+  storage_class = var.bucket.storage_class
+  project       = var.project
+
+  versioning {
+    enabled = true
+  }
+
+  uniform_bucket_level_access = true
+}
+
+
+resource "google_service_account" "vm_sa" {
+  account_id   = var.sa.account_id
+  display_name = var.sa.display_name
+}
+
+resource "google_project_iam_member" "bucket_access" {
+  project = var.project
+  role    = "roles/storage.objectViewer"
+  member  = "serviceAccount:${google_service_account.vm_sa.email}"
+}
+
+
+resource "google_compute_firewall" "ssh-rule" {
+  name    = var.firewall_rule
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
 }
---
+++ variables.tf
@@ -0,0 +1,46 @@
+variable "project" {}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-c"
+}
+
+variable "vpc" {
+  description = "vpc network"
+  type        = string
+  default     = "terraform-network"
+}
+
+variable "vm" {
+  description = "vm configuration"
+  type = object({
+    name         = string
+    machine_type = string
+    tags         = list(string)
+    image        = string
+  })
+}
+
+variable "bucket" {
+  description = "bucket configuration"
+  type = object({
+    name          = string
+    location      = string
+    storage_class = string
+  })
+}
+
+variable "sa" {
+  description = "sa configuration"
+  type = object({
+    account_id   = string
+    display_name = string
+  })
+}
+
+variable "firewall_rule" {
+  default = "ssh"
+}
